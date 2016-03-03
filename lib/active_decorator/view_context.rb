2ec6818df2042d1466feaedd1e14109a301202dd
Chaerim Yeo <yeochaerim@gmail.com>
2016-03-03 09:58:06 +0900

    Take request_store instead of thread

--- lib/active_decorator/view_context.rb
+++ lib/active_decorator/view_context.rb
@@ -1,17 +1,19 @@
+require 'request_store'
+
 module ActiveDecorator
   module ViewContext
     class << self
       def current
-        Thread.current[:active_decorator_view_contexts].last
+        RequestStore.store[:active_decorator_view_contexts].last
       end
 
       def push(view_context)
-        Thread.current[:active_decorator_view_contexts] ||= []
-        Thread.current[:active_decorator_view_contexts] << view_context
+        RequestStore.store[:active_decorator_view_contexts] ||= []
+        RequestStore.store[:active_decorator_view_contexts] << view_context
       end
 
       def pop
-        Thread.current[:active_decorator_view_contexts].pop if Thread.current[:active_decorator_view_contexts]
+        RequestStore.store[:active_decorator_view_contexts].pop if RequestStore.store[:active_decorator_view_contexts]
       end
     end
 
