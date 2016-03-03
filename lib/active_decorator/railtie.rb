9afafae157b87ee229fd7a1e9fbe3159635c8ee0
Chaerim Yeo <yeochaerim@gmail.com>
2016-03-03 11:41:42 +0900

    Apply monkey-patch of TemplateRenderer

--- lib/active_decorator/railtie.rb
+++ lib/active_decorator/railtie.rb
@@ -7,6 +7,8 @@ module ActiveDecorator
       ActiveSupport.on_load :action_view do
         require 'active_decorator/monkey/action_view/partial_renderer'
         ActionView::PartialRenderer.send :prepend, ActiveDecorator::Monkey::ActionView::PartialRenderer
+        require 'active_decorator/monkey/action_view/template_renderer'
+        ActionView::TemplateRenderer.send :prepend, ActiveDecorator::Monkey::ActionView::TemplateRenderer
       end
 
       ActiveSupport.on_load :action_controller do
