f66ffa352a51b2fd7fc066d0e5c36278fe2cd84f
Chaerim Yeo <yeochaerim@gmail.com>
2016-03-03 11:46:00 +0900

    Refactor code

--- lib/active_decorator/monkey/action_view/template_renderer.rb
+++ lib/active_decorator/monkey/action_view/template_renderer.rb
@@ -3,14 +3,8 @@ module ActiveDecorator
     module ActionView
       module TemplateRenderer
         def render(context, options)
-          assigns = context.assigns
-          assigns.values.each do |assign|
-            ActiveDecorator::Decorator.instance.decorate assign
-          end
-          context.assign(assigns)
-          options.values.each do |option|
-            ActiveDecorator::Decorator.instance.decorate option
-          end
+          context.assigns.values.each { |assign| ActiveDecorator::Decorator.instance.decorate assign }
+          options.values.each { |option| ActiveDecorator::Decorator.instance.decorate option }
           super
         end
       end
